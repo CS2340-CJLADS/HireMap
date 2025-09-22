743acb9faf66dfbe062439755521e18d940b6287
Sandro <sandroqarqusashvili@gmail.com>
2025-09-22 17:56:03 -0400

    commit 1

--- HireMap/urls.py
+++ HireMap/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView, RedirectView
-
-SPA = TemplateView.as_view(template_name="index.html")
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     # Admin first (and force /admin -> /admin/)
@@ -14,7 +12,4 @@ urlpatterns = [
 
     # React SPA routes (explicit)
     path("", include(("home.urls", "home"), namespace="home")),
-
-    # Catch-all for front-end routes EXCEPT server prefixes
-    re_path(r"^(?!admin/|api/|static/|media/).*$", SPA),
 ]
--- home/urls.py
+++ home/urls.py
@@ -2,17 +2,26 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 app_name = "home"
-SPA = TemplateView.as_view(template_name="index.html")
+HOME = TemplateView.as_view(template_name="home.html")
+JOBS_LIST = TemplateView.as_view(template_name="jobs_list.html")
+JOB_DETAIL = TemplateView.as_view(template_name="job_detail.html")
+APPLICATIONS = TemplateView.as_view(template_name="applications_board.html")
+PROFILE = TemplateView.as_view(template_name="profile_edit.html")
+RECRUITER_JOBS = TemplateView.as_view(template_name="recruiter_jobs.html")
+RECRUITER_JOB_NEW = TemplateView.as_view(template_name="recruiter_job_new.html")
+SAVED_SEARCHES = TemplateView.as_view(template_name="saved_searches.html")
+MESSAGES = TemplateView.as_view(template_name="messages_inbox.html")
+MAP = TemplateView.as_view(template_name="map.html")
 
 urlpatterns = [
-    path("", SPA, name="index"),
-    path("jobs/", SPA, name="jobs_list"),
-    path("jobs/<int:pk>/", SPA, name="job_detail"),
-    path("applications/", SPA, name="applications_board"),
-    path("profile/", SPA, name="profile_edit"),
-    path("recruiter/jobs/", SPA, name="recruiter_jobs"),
-    path("recruiter/jobs/new/", SPA, name="recruiter_job_new"),
-    path("saved-searches/", SPA, name="saved_searches"),
-    path("messages/", SPA, name="messages_inbox"),
-    path("map/", SPA, name="map"),
+    path("", HOME, name="index"),
+    path("jobs/", JOBS_LIST, name="jobs_list"),
+    path("jobs/<int:pk>/", JOB_DETAIL, name="job_detail"),
+    path("applications/", APPLICATIONS, name="applications_board"),
+    path("profile/", PROFILE, name="profile_edit"),
+    path("recruiter/jobs/", RECRUITER_JOBS, name="recruiter_jobs"),
+    path("recruiter/jobs/new/", RECRUITER_JOB_NEW, name="recruiter_job_new"),
+    path("saved-searches/", SAVED_SEARCHES, name="saved_searches"),
+    path("messages/", MESSAGES, name="messages_inbox"),
+    path("map/", MAP, name="map"),
 ]
